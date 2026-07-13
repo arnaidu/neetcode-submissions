5ca2f6c3e041d0d1314486cb1ac89622669f28b0
arnaidu <39361926+arnaidu@users.noreply.github.com>
2026-07-13 15:21:55 -0400

    Add: reverse-string - submission-2

---
+++ Data Structures & Algorithms/reverse-string/submission-2.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public void ReverseString(char[] s) {
+        var left = 0;
+        var right = s.Length - 1;
+        while (left < right) {
+            (s[left], s[right]) = (s[right], s[left]);
+            left++;
+            right--;
+        }
+    }
+}
