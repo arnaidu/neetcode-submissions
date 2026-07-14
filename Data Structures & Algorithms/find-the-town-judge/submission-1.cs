dac9dfcdfbe222cf105739de7b597649c8dda09b
arnaidu <39361926+arnaidu@users.noreply.github.com>
2026-07-14 16:26:34 -0400

    Add: find-the-town-judge - submission-1

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-1.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int FindJudge(int n, int[][] trust) {
+        // This is a start graph which edges inwards to a center node being the judge
+        int[] inDegree = new int[n];
+        int[] outDegree = new int[n];
+        foreach (var edge in trust) {
+            var from = edge[0];
+            var to = edge[1];
+
+            inDegree[to - 1]++;
+            outDegree[from - 1]++;
+        }
+
+        foreach (var i in inDegree) {
+            Console.WriteLine(i);
+        }
+        if (trust.Length > 0 && inDegree[trust[0][1] - 1] == n - 1 && outDegree[trust[0][1] - 1] == 0) {
+            return trust[0][1];
+        }
+
+        return -1;
+    }
+}
